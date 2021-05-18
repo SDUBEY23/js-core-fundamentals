51ba3159c97702f87c3ef30ef3678808632abf73
Swapnil <swapnil.vishu.7027@gmail.com>
2021-05-18 19:00:42 +0530

    spelling fix

--- pollyfillForBind.js
+++ pollyfillForBind.js
@@ -1,26 +1,32 @@
-let person={
-    firstname:"Swapnil",
-    lastname:"Dubey"
-}
+let person = {
+  firstname: "Swapnil",
+  lastname: "Dubey",
+};
 
-let printName = function(hometown, state, country){
-    console.log(this.firstname + ' ' + this.lastname + ' , '+ hometown+ ", "+state+" , "+ country);
-}
+let printName = function (hometown, state, country) {
+  console.log(
+    this.firstname +
+      " " +
+      this.lastname +
+      " , " +
+      hometown +
+      ", " +
+      state +
+      " , " +
+      country
+  );
+};
 
 let printMyname = printName.bind(person, "Bareilly");
 printMyname("UP", "India");
 
-
-Function.prototype.myBind = function(...args){
-    let obj = this; // this points to printName function
-    params = args.slice(1);
-    return function(...args2){
-        obj.apply(args[0], [...params, ...args2]); // conatinates to a larger array
-    }
-}
-
+Function.prototype.myBind = function (...args) {
+  let obj = this; // this points to printName function
+  params = args.slice(1);
+  return function (...args2) {
+    obj.apply(args[0], [...params, ...args2]); // concatinates to a larger array
+  };
+};
 
 let printMyname2 = printName.myBind(person, "Bareilly");
 printMyname2("UP", "India");
-
-
